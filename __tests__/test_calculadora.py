6cd505920557062f12ed7f6910704b45ce6639fb
CristiniR <cristini.rafaela@hotmail.com>
2025-03-26 10:03:05 -0300

    Calculadora básica + testes de unidade simples e Data Driven

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -5,8 +5,9 @@ import pytest
 # Funções que serão testadas...................................................................................................
 
 from Calculadora.calculadora import somar_dois_numeros, subtrair_dois_numeros, multiplicar_dois_numeros, dividir_dois_numeros
+from utils.utils import ler_csv
 
-# Testes.......................................................................................................................
+# Tests........................................................................................................................
 
 def test_somar_dois_numeros():
     num1 = 5
@@ -45,5 +46,42 @@ def test_dividir_dois_numeros():
 
     resultado_obtido = dividir_dois_numeros(num1, num2)
 
+    
+def teste_dividir_por_zero():
+    num1 = 10
+    num2 = 0
+    resultado_esperado = 'Erro: Não é possível dividir por zero' 
+
+    resultado_obtido = dividir_dois_numeros(num1, num2)
+
     assert resultado_esperado == resultado_obtido
 
+# Data Driven Tests (TDD).......................................................................................................
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado',
+                         [
+                           (5, 7, 12),
+                           (0, 8, 8),
+                           (10, -15, -5),
+                           (6, 0.75, 6.75),
+                         ]
+                        )
+
+def test_somar_dois_numeros_lista(num1, num2, resultado_esperado):    
+
+    resultado_obtido = somar_dois_numeros(num1, num2)
+
+    assert resultado_esperado == resultado_obtido
+
+
+# Usando massa de dados CSV.....................................................................................................
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado',
+                        ler_csv('./fixtures/massa_somar.csv')
+                        )
+
+def test_somar_dois_numeros_csv(num1, num2, resultado_esperado):    
+
+    resultado_obtido = somar_dois_numeros (float(num1), float(num2))
+
+    assert float(resultado_esperado) == resultado_obtido
