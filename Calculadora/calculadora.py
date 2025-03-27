fc2e1f73d0bd182b6d7344ff6e759a18f2ea5264
CristiniR <cristini.rafaela@hotmail.com>
2025-03-27 09:58:14 -0300

    Calculadora básica + testes de unidade simples, Data Driven e utilizando dados CSV

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -12,4 +12,9 @@ def dividir_dois_numeros(num1, num2):
         return num1 / num2
     except(ZeroDivisionError):
         return 'Erro: Não é possível dividir por zero'
+    
+def calcular_area_do_retangulo(largura, comprimento):
+    return largura * comprimento
 
+def calcular_area_do_triangulo(largura, comprimento):
+    return largura * comprimento / 2
--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -4,7 +4,7 @@ import pytest
 
 # Funções que serão testadas...................................................................................................
 
-from Calculadora.calculadora import somar_dois_numeros, subtrair_dois_numeros, multiplicar_dois_numeros, dividir_dois_numeros
+from Calculadora.calculadora import somar_dois_numeros, subtrair_dois_numeros, multiplicar_dois_numeros, dividir_dois_numeros, calcular_area_do_retangulo, calcular_area_do_triangulo
 from utils.utils import ler_csv
 
 # Tests........................................................................................................................
@@ -46,6 +46,8 @@ def test_dividir_dois_numeros():
 
     resultado_obtido = dividir_dois_numeros(num1, num2)
 
+    assert resultado_esperado == resultado_obtido
+
     
 def teste_dividir_por_zero():
     num1 = 10
@@ -55,6 +57,27 @@ def teste_dividir_por_zero():
     resultado_obtido = dividir_dois_numeros(num1, num2)
 
     assert resultado_esperado == resultado_obtido
+
+
+def testar_calcular_area_do_retangulo():
+    largura = 5
+    comprimento = 10
+    resultado_esperado = 50  
+
+    resultado_obtido = calcular_area_do_retangulo(largura, comprimento)
+
+    assert resultado_esperado == resultado_obtido
+
+
+def testar_calcular_area_do_triangulo():
+    largura = 5
+    comprimento = 10
+    resultado_esperado = 25  
+
+    resultado_obtido = calcular_area_do_triangulo(largura, comprimento)
+
+    assert resultado_esperado == resultado_obtido
+ 
 
 # Data Driven Tests (TDD).......................................................................................................
 
@@ -74,6 +97,22 @@ def test_somar_dois_numeros_lista(num1, num2, resultado_esperado):
     assert resultado_esperado == resultado_obtido
 
 
+@pytest.mark.parametrize('largura, comprimento, resultado_esperado',
+                         [
+                           (5, 7, 35),
+                           (6, 8, 48),
+                           (10, 15, 150),
+                           (6, 0.75, 4.50),
+                         ]
+                        )
+
+def testar_calcular_area_do_retangulo(largura, comprimento, resultado_esperado):
+    
+    resultado_obtido = calcular_area_do_retangulo(largura, comprimento)
+
+    assert resultado_esperado == resultado_obtido
+
+
 # Usando massa de dados CSV.....................................................................................................
 
 @pytest.mark.parametrize('num1, num2, resultado_esperado',
@@ -83,5 +122,16 @@ def test_somar_dois_numeros_lista(num1, num2, resultado_esperado):
 def test_somar_dois_numeros_csv(num1, num2, resultado_esperado):    
 
     resultado_obtido = somar_dois_numeros (float(num1), float(num2))
+
+    assert float(resultado_esperado) == resultado_obtido
+
+
+@pytest.mark.parametrize('largura, comprimento, resultado_esperado',
+                        ler_csv('./fixtures/massa_retangulo.csv')
+                        )
+
+def testar_calcular_area_do_retangulo(largura, comprimento, resultado_esperado):
+    
+    resultado_obtido = calcular_area_do_retangulo(float(largura), float(comprimento))
 
     assert float(resultado_esperado) == resultado_obtido
